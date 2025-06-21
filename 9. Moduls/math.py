82ab1c6500844270052cb81e48c74699e364d56e
Birgül Yüksel <birgulalpyuksel@gmail.com>
2025-06-21 23:28:05 +0200

    Modüller Çalışıldı.

---
+++ 9. Moduls/math.py
@@ -0,0 +1,18 @@
+# yöntem 1
+import math as islem
+
+value = islem.sqrt(49)
+value = islem.factorial(5)
+value = islem.floor(5.9)
+
+print(value)
+
+# Yöntem 2
+def sqrt(x):
+    print("x: " + str(x))
+    
+from math import *
+
+value = factorial(5)
+value = sqrt(9)
+value = ceil(9.8)
---
+++ 9. Moduls/mod.py
@@ -0,0 +1,20 @@
+
+print("modül eklendi")
+
+number = 20
+
+numbers= [1,2,3]
+
+person = {
+    "name":"berke",
+    "age":"27",
+    "city":"istanbul"
+}
+
+def func(x):
+    print(f"x: {x}")
+
+
+class Person:
+    def speak(self):
+        print("I am speaking.")
